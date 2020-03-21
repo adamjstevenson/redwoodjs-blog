7a8d705843921acfeed909f77a38ac094c256759
Adam Stevenson <adamjstevenson@gmail.com>
2020-03-21 15:03:16 -0700

    create contacts model and mutation

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -5,17 +5,46 @@ import {
   TextAreaField,
   FieldError,
   Label,
+  FormError,
 } from '@redwoodjs/web'
+import { useMutation } from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
 
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
-const ContactPage = () => {
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: ContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+const ContactPage = (props) => {
+  const formMethods = useForm({ mode: 'onBlur' })
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      alert('Thanks, got it!')
+      formMethods.reset()
+    },
+  })
   const onSubmit = (data) => {
+    create({ variables: { input: data } })
     console.log(data)
   }
   return (
     <BlogLayout>
-      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+      <Form
+        onSubmit={onSubmit}
+        validation={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError
+          error={error}
+          wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+          listStyle={{ 'list-style-type': 'none' }}
+        />
         <Label
           name="name"
           style={{ display: 'block' }}
@@ -30,7 +59,6 @@ const ContactPage = () => {
           validation={{ required: true }}
         />
         <FieldError name="name" style={{ color: 'red' }} />
-
         <Label
           name="email"
           style={{ display: 'block' }}
@@ -44,14 +72,9 @@ const ContactPage = () => {
           errorStyle={{ display: 'block', borderColor: 'red' }}
           validation={{
             required: true,
-            pattern: {
-              value: /[^@]+@[^\.]+\..+/,
-              message: 'Please enter a valid email address',
-            },
           }}
         />
         <FieldError name="email" style={{ color: 'red' }} />
-
         <Label
           name="message"
           style={{ display: 'block' }}
@@ -66,8 +89,9 @@ const ContactPage = () => {
           validation={{ required: true }}
         />
         <FieldError name="message" style={{ color: 'red' }} />
-
-        <Submit style={{ display: 'block' }}>Save</Submit>
+        <Submit style={{ display: 'block' }} disabled={loading}>
+          Save
+        </Submit>
       </Form>
     </BlogLayout>
   )
